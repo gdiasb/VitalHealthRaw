7d8504799c85f241d9916222fa19a1f6838927ad
Gabriela <diasgbm@gmail.com>
2022-12-05 21:07:33 -0300

    Create Project

---
+++ src/main/java/com/gdias/vitalhealth/VitalHealthApplication.java
@@ -0,0 +1,13 @@
+package com.gdias.vitalhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VitalHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdias/vitalhealth/VitalHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdias.vitalhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
